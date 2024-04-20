a14dd4a6df30fea0fbdfce59ef1b222520f4cec3
Shuyuan Zheng <127867140+EAsUpluckYX@users.noreply.github.com>
2024-04-19 21:22:16 -0400

    Add test for data

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,30 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw data so that it can be analyzed.
+# Author: Shuyuan Zheng
+# Date: 10 April 2024
+# Contact: Sheldon.Zheng@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: Need to have manually downloaded the data.
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(arrow)
 
-#### Test data ####
+#### Clean data ####
+data <- read_csv("data/raw_data/California Housing Prices_1597_1.csv")
+
+data <- data |>
+  mutate(
+    total_bedrooms = if_else(is.na(total_bedrooms), mean(total_bedrooms, na.rm = TRUE), total_bedrooms)
+  )
+
+numeric_columns <- sapply(data, is.numeric)
+data <- data |>
+  mutate(across(where(is.numeric), ~if_else(is.na(.), mean(., na.rm = TRUE), .)))
+
+column_means <- colMeans(select(data, where(is.numeric)), na.rm = TRUE)
+print(column_means)
+
+#### Save data ####
+write_csv(data, "data/analysis_data/cleaned_data.csv")
+write_parquet(data, "data/analysis_data/cleaned_data.parquet")
