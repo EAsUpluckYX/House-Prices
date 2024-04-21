8f36d7e938d0ceff95c1cbb38f30ba6bdd01d73b
Shuyuan Zheng <127867140+EAsUpluckYX@users.noreply.github.com>
2024-04-21 02:18:47 -0400

    Update test data

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -12,24 +12,30 @@ library(arrow)
 library(tidyverse)
 
 #### Test data ####
-cleaned_data <- read_parquet("data/analysis_data/cleaned_data.parquet")
+cleaned_data <- read_parquet("data/analysis_data/trainfinal.parquet")
+cleaned_data1 <- read_parquet("data/analysis_data/testfinal.parquet")
 
 # Test expectations for 'median_house_value'
 # Expect that values are within a reasonable range for California housing.
 all(cleaned_data$median_house_value >= 10000 & cleaned_data$median_house_value <= 1000000)
+all(cleaned_data1$median_house_value >= 10000 & cleaned_data$median_house_value <= 1000000)
 
 # Test expectations for 'total_bedrooms'
 # Expect that there are no negative values for bedrooms.
 all(cleaned_data$total_bedrooms >= 0)
+all(cleaned_data1$total_bedrooms >= 0)
 
 # Test expectations for 'ocean_proximity'
 # Expect that this categorical variable only contains specific categories.
 all(cleaned_data$ocean_proximity %in% c("NEAR BAY", "INLAND", "<1H OCEAN", "NEAR OCEAN", "ISLAND"))
+all(cleaned_data1$ocean_proximity %in% c("NEAR BAY", "INLAND", "<1H OCEAN", "NEAR OCEAN", "ISLAND"))
 
 # Test expectations for 'house_median_age'
 # Expect that the age of the house is between 0 and 100 years.
 all(cleaned_data$housing_median_age >= 0 & cleaned_data$housing_median_age <= 100)
+all(cleaned_data1$housing_median_age >= 0 & cleaned_data$housing_median_age <= 100)
 
 # Test data integrity
 # Expect that there are no missing values in key columns.
 all(complete.cases(cleaned_data))
+all(complete.cases(cleaned_data1))
