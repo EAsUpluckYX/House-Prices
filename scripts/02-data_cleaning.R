6e7de059cf963d80454eb5b8310ac3e09c88c55c
Shuyuan Zheng <127867140+EAsUpluckYX@users.noreply.github.com>
2024-04-21 02:15:18 -0400

    Update data cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -13,18 +13,25 @@ library(arrow)
 #### Clean data ####
 data <- read_csv("data/raw_data/California Housing Prices_1597_1.csv")
 
-data <- data |>
+data2 <- data |>
   mutate(
     total_bedrooms = if_else(is.na(total_bedrooms), mean(total_bedrooms, na.rm = TRUE), total_bedrooms)
   )
 
 numeric_columns <- sapply(data, is.numeric)
-data <- data |>
-  mutate(across(where(is.numeric), ~if_else(is.na(.), mean(., na.rm = TRUE), .)))
-
-column_means <- colMeans(select(data, where(is.numeric)), na.rm = TRUE)
+numeric_data <- data[, numeric_columns]
+column_means <- colMeans(numeric_data, na.rm = TRUE)
 print(column_means)
 
+#### Grab a portion of the data as train data and test data. ####
+rows <- sample(1:20641, 641, replace=FALSE) 
+train<- data2[rows,]
+test=data2[-rows,]
+
+
 #### Save data ####
-write_csv(data, "data/analysis_data/cleaned_data.csv")
-write_parquet(data, "data/analysis_data/cleaned_data.parquet")
+write_csv(train, "trainfinal.csv")
+write_csv(test, "testfinal.csv")
+
+write_parquet(train, "trainfinal.parquet")
+write_parquet(test, "testfinal.parquet")
